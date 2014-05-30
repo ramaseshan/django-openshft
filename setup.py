4480972934b5835cf1bd9badafc0f383a683e12d
ramaseshan <ramaseshanmukund@gmail.com>
2014-05-30 11:52:45 +0530

    Django github configuration

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='school',
+    version='1.0',
+    description='OpenShift App',
+    author='Ramaseshan',
+    author_email='ram.seshan.cs@gmail.com',
+    url='http://www.python.org/sigs/distutils-sig/',
+    install_requires=['Django>=1.6'],
+)
